fb1589cc4a66779704490d14afb058a389fea94a
ncreea <csaba.nyiro@reea.net>
2019-04-23 04:53:55 -0400

    Countable warning fix in RiakObject

--- src/Basho/Riak/RiakObject.php
+++ src/Basho/Riak/RiakObject.php
@@ -956,7 +956,7 @@ class RiakObject
      */
     public function getSiblingCount()
     {
-        return count($this->siblings);
+        return (isset($this->siblings)&&is_array($this->siblings))?count($this->siblings):0;
     }
 
     /**
